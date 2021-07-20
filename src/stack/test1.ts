073520624c608f3ff32304374f5fd0c023af221d
yijun-blockin <yijun.wu@block.com>
2021-07-20 21:17:28 +0800

    leetcode - 20 有效的括号

---
+++ src/stack/test1.ts
@@ -0,0 +1,26 @@
+/**
+ * leetcode - 20 有效的括号
+ */
+
+import Stack from './index';
+export const isValid = (str: string): boolean => {
+  const stack = new Stack();
+  const strMap: any = {
+    ')': '(',
+    ']': '[',
+    '}': '{',
+  };
+  for (let i of str) {
+    if (['(', '[', '{'].includes(i)) {
+      stack.push(i);
+    }
+    if (strMap[i]) {
+      if (strMap[i] === stack.peek()) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.size() === 0;
+}
